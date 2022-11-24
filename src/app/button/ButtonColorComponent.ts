0f41ff630e383242d0c26da7baef7c1460a9e7c0
Kagol <kagol@sina.com>
2022-11-24 16:21:53 +0800

    init project

---
+++ src/app/button/ButtonColorComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './button-color.html',
+})
+export class ButtonColorComponent {
+  public sayHello(): void {
+    alert('hello');
+  }
+}
